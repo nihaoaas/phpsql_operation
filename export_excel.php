664e67c636eb9d57b8d2c3917dded9b0ca039459
nihaoaas <1159796599@qq.com>
2018-01-26 09:59:45 +0800

    数据库到处excel

---
+++ export_excel.php
@@ -0,0 +1,16 @@
+<?php
+$con = new mysqli('localhost', 'root', '', 'collectmail');
+$con->set_charset('utf8');
+$sql = "SELECT * FROM mail_data";
+$stmt = $con->prepare($sql);
+// $stmt->bind_param('s', $mail);
+// $mail = 'sdfa@qq.com';
+$stmt->execute();
+$result = $stmt->get_result();
+$arr = $result->fetch_all(MYSQLI_ASSOC);//$result->fetch_array(MYSQLI_ASSOC) 单行
+$stmt->close();
+$f = fopen('data.xls','w');
+foreach ($arr as $v) {
+	fwrite($f,$v['id']."\t".$v['mail']."\t".date('y-m-d H:m:s',$v['create_time'])."\t\n");
+}
+fclose($f);
